95211095a0ec4f185fceafdddcfae29d6e6830cd
lefeurr <feenandokurniawan@gmail.com>
2022-09-23 02:04:48 +0700

    no 3

---
+++ app/src/main/java/com/example/tubespbp/room/User.kt
@@ -0,0 +1,15 @@
+package com.example.tubespbp.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val username: String,
+    val password: String,
+    val email: String,
+    val birth: String,
+    val phone: String
+)

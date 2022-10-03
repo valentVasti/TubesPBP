0670c73d4f91f08107b648c093b8db29d0881406
lefeurr <feenandokurniawan@gmail.com>
2022-10-03 21:44:39 +0700

    .

--- app/src/main/java/com/example/tubespbp/MainActivity.kt
+++ app/src/main/java/com/example/tubespbp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.tubespbp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -9,6 +14,10 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
@@ -16,10 +25,11 @@ import com.google.android.material.textfield.TextInputLayout
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-
+    private val logChannel = "logChannel"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
@@ -92,6 +102,7 @@ class MainActivity : AppCompatActivity() {
             if(!checkLogin)
                 return@OnClickListener
             else{
+                sendNotification()
                 val moveHome = Intent(this, HomeActivity::class.java)
                 startActivity(moveHome)
             }
@@ -103,6 +114,45 @@ class MainActivity : AppCompatActivity() {
 
         if(loginData!=null) {
             editUsername.setText(loginData.getString("username"))
+        }
+    }
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+
+            val registChannel = NotificationChannel(logChannel, name, NotificationManager.IMPORTANCE_DEFAULT).apply{
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(registChannel)
+        }
+    }
+
+    private fun sendNotification(){
+
+        val intent: Intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val bigPictureBitmap = ContextCompat.getDrawable(this, R.drawable.big_picture)?.toBitmap()
+        val bigPictureStyle = NotificationCompat.BigPictureStyle().bigPicture(bigPictureBitmap)
+
+        //val broadcastIntent: Intent = Intent(this, NotificationReceiver::class.java)
+        //broadcastIntent.putExtra("toastMessage", binding?.etMessage?.text.toString())
+        val builder = NotificationCompat.Builder(this, logChannel)
+            .setSmallIcon(R.drawable.ic_baseline_check_circle_24)
+            .setContentTitle("Login Success!")
+            .setContentText("")
+            .setStyle(bigPictureStyle)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(101, builder.build())
         }
     }
 }

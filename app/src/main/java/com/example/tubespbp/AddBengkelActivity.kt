0118dc99bb7817bf54830e3406d876ae6c259d9d
melsiorsanjaya <bandasoojayaa@gmail.com>
2022-11-20 23:29:38 +0700

    UGD

---
+++ app/src/main/java/com/example/tubespbp/AddBengkelActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tubespbp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tubespbp.databinding.ActivityRegistActiviyBinding
+import com.example.tubespbp.room.UserDB
+import com.google.android.gms.common.util.Clock
+
+class AddBengkelActivity : AppCompatActivity() {
+
+    private var clock = Clock.getInstance()
+    val  db by lazy { UserDB(this) }
+    private var noteId: Int = 0
+    private val regChannel = "regChannel"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityRegistActiviyBinding = ActivityRegistActiviyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputivBengkel = binding.inputLayoutEmail
+    }
+}
